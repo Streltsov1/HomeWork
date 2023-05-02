25cb0911f1c798bf6d9b68a8adf7a799d023fea1
Oleg Streltsov <olegstrelsov2003@gmail.com>
2023-05-03 00:39:57 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    enum VagonType { Tank = 0, Passenger, Restaurant }
+    struct Vagon
+    {
+        readonly private int number;
+        public int Number => number;
+        private const int seatsNumber = 25;
+        public int SeatsNumber => seatsNumber;
+        public int Passengers { get; set; }
+        public VagonType Type { get; set; }
+        public Vagon(int num, int passenger, VagonType type)
+        {
+            number = num;
+            Passengers = passenger;
+            Type = type;
+        }
+        public int FreeSeats() => seatsNumber - Passengers;
+    }
+    enum TrainType { Freight=0, Passenger, Mail_Baggage }
+    class Train
+    {
+        readonly private int number;
+        public int Number => number;
+        public TrainType Type { get; set; }
+        public string RouteName { get; set; }
+        public DateTime DispatchDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        private Vagon[] vagons;
+        private int vagonNumber = 0;
+        public int VagonNumber => vagonNumber;
+
+        public Train(int num, TrainType type, string routeName, DateTime dispatch, DateTime arrival)
+        {
+            number = num;
+            Type = type;
+            RouteName = routeName;
+            DispatchDate = dispatch;
+            ArrivalDate = arrival;
+        }
+        public void AddVagon(Vagon vagon)
+        {
+            vagonNumber++;
+            Vagon[] new_vagon = new Vagon[vagonNumber];
+            for (int i = 0; i < vagonNumber - 1; i++)
+            {
+                new_vagon[i] = vagons[i];
+            }
+            new_vagon[vagonNumber - 1] = vagon;
+            vagons = new_vagon;
+        }
+        public TimeSpan TimeLeft() => ArrivalDate.Subtract(DispatchDate);
+        public override string ToString()
+        {
+            return $"This is {Type} train #{number} current route {RouteName}, time of dispatch {DispatchDate}, time of arrival {ArrivalDate}, number of vagons {vagonNumber}";
+        }
+        public int FreeSeats()
+        {
+            int free = 0;
+            for (int i = 0; i < vagonNumber; i++)
+            {
+                free += vagons[i].FreeSeats();
+            }
+            return free;
+        }
+        public double AvgPassengers()
+        {
+            double avg = 0;
+            for (int i = 0; i < vagonNumber; i++)
+            {
+                avg += vagons[i].Passengers;
+            }
+            return avg/VagonNumber;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vagon vagon1 = new Vagon(1, 10, VagonType.Passenger);
+            Vagon vagon2 = new Vagon(2, 20, VagonType.Passenger);
+            Vagon vagon3 = new Vagon(3, 3, VagonType.Restaurant);
+            Vagon vagon4 = new Vagon(4, 0, VagonType.Tank);
+            Train train = new Train(123, TrainType.Passenger,"Rivne-Kiyv", DateTime.Now, DateTime.Now.AddHours(10));
+            train.AddVagon(vagon1);
+            train.AddVagon(vagon2);
+            train.AddVagon(vagon3);
+            train.AddVagon(vagon1);
+            train.AddVagon(vagon1);
+            train.AddVagon(vagon3);
+            Console.WriteLine(train.ToString());
+            Console.WriteLine($"time to arrival {train.TimeLeft() }");
+            Console.WriteLine($"Number of vagons {train.VagonNumber}, free seats {train.FreeSeats()}, average number of passenger {train.AvgPassengers()}");
+            train.RouteName = "Rivne-Dnipro";
+            train.ArrivalDate = DateTime.Now.AddHours(15);
+            Console.WriteLine(train.ToString());
+
+        }
+    }
+}
---
+++ String/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace C_Sharp_String
+{
+    public static class Revers
+    {
+        public static string reverse(this string str)
+        {
+            return new string(str.Reverse().ToArray());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            // завдання 1
+            string text = "12345";
+            string reverse = text.reverse();
+            Console.WriteLine($"Нормальне слово: {text}\nСлово в зворотньому порядку: {reverse}");
+            // завдання 2
+            Console.Write("Введіть текст: ");
+            text = Console.ReadLine();
+            try
+            {
+                Console.WriteLine(text.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries)[1]);
+            }
+            catch
+            {
+                Console.WriteLine("Не знайдено точок у строці");
+            }
+            // завдання 3
+            Console.Write("Введіть слово: ");
+            text = Console.ReadLine();
+            reverse = text.reverse();
+            if (text.ToLower() == reverse.ToLower())
+                Console.WriteLine("Це слово паліндром");
+            else
+                Console.WriteLine("Це слово не є паліндромом");
+            // завдання 4
+            Console.Write("Введіть текст: ");
+            text = Console.ReadLine();
+            double upper = 0;
+            double lower = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsUpper(text[i]))
+                    upper++;
+                else if (char.IsLower(text[i]))
+                    lower++;
+            }
+            Console.WriteLine($"Великих літер {100.0 / text.Length * upper}%, Малих літер {100.0 / text.Length * lower}%");
+            // завдання 5
+            Console.Write("Введіть текст: ");
+            text = Console.ReadLine();
+            string[] string_arr = text.Split();
+            int index = 0;
+            do
+            {
+                Console.Write($"Введіть номер слова, номер повинен бути в межах(1 - {string_arr.Length}): ");
+                index = int.Parse(Console.ReadLine());
+            } while (index < 1 || index > string_arr.Length);
+            Console.WriteLine(string_arr[index - 1].First());
+            // завдання 6
+            Console.Write("Введіть текст: ");
+            text = Console.ReadLine();
+            string_arr = text.Split();
+            for (int i = 0; i < string_arr.Length; i++)
+            {
+                if (string_arr[i].Length > 5)
+                {
+                    for (int j = 5; j < string_arr[i].Length; j++)
+                    {
+                        string_arr[i] = string_arr[i].Remove(j, 1).Insert(j, "$");
+                    }
+                }
+            }
+            foreach (var item in string_arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
